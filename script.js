521462ab0fbca81d35350a6fb6f15aa62e6f7c2c
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-10-10 16:54:21 -0300

    Rendering workout input form

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict';
 
+const workout = document.querySelector('.workout');
+
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputType = document.querySelector('.form__input--type');
+const inputElevation = document.querySelector('.form__input--elev');
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let map;
+let mapEvent;
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -11,26 +22,18 @@ if (navigator.geolocation) {
 
             const coords = [latitude, longitude];
 
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            map.on('click', function(mapEvent) {
-                const {lat, lng} = mapEvent.latlng;
+            // Handling clicks on map
+            map.on('click', function(mapE) {
+                workout.classList.remove('hidden');
+                inputDistance.focus();
 
-                L.marker([lat, lng])
-                 .addTo(map)
-                 .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                 }))
-                 .setPopupContent('Workout')
-                 .openPopup();
+                mapEvent = mapE;
             });
         }, 
     
@@ -39,3 +42,30 @@ if (navigator.geolocation) {
         }
     );
 }
+
+workout.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputCadence.value = inputDuration.value = '';
+
+    // Display marker
+    const {lat, lng} = mapEvent.latlng;
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+inputType.addEventListener('change', function() {
+    inputElevation.closest('.form__inputs').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__inputs').classList.toggle('form__row--hidden');
+});
